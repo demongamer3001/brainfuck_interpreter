d65757c57fb24ae70669a4f9d7f3571811c6d0c6
= <=>
2022-03-24 13:15:47 +0100

    Done

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-import keyboard, sys
+import keyboard, sys, os
 from time import sleep
 
-with open("code.bf", "r") as f:
-    script = f.read()
+file_name = "code.bf"
+
+try:
+    file_name = sys.argv[1]
+except:
+    pass
+
+
+file_name = "./scripts/" + file_name
+
+script = ""
+with open(file_name, "r") as f:
+    for _ in f.read():
+        if _ in "<>+-.,[]":
+            script += _
     script_length = len(script)
 
 program_pointer = 0
 pointer = 0
 tape = []
-tape_size = 16
+loop_stack = []
+
+
+output_ascii = True
+tape_size = 8
 
 for _ in range(tape_size):
     tape.append(0)
@@ -16,13 +33,50 @@ for _ in range(tape_size):
 while True:
     if program_pointer == script_length:
         sys.stdout.flush()
-        exit(0)
+        break
     
     _ = script[program_pointer]
 
+    if _ == "<":
+        pointer -= 1
+    elif _ == ">":
+        pointer += 1
+
+    elif _ == "+":
+        tape[pointer] += 1
+    elif _ == "-":
+        tape[pointer] -= 1
+    
+    elif _ == ".":
+        if output_ascii:
+            print(chr(tape[pointer]), end = "")
+            sys.stdout.flush()
+        else:
+            print(tape[pointer])
+        sleep(0.05)
+    elif _ == ",":
+        tape[pointer] = ord(keyboard.read_key()) # Wait until a key is pressed
+        print() # Make a newline
+    
+    elif _ == "[":
+        loop_stack.append(program_pointer + 1)
+    elif _ == "]":
+        if tape[pointer] != 0:
+            program_pointer = loop_stack[-1] - 1
+        else:
+            loop_stack.pop()
+
+
+    if pointer < 0:
+        pointer = tape_size - 1
+    elif pointer > tape_size - 1:
+        pointer = 0
+
     if tape[pointer] < 0:
         tape[pointer] = 255
-    if tape[pointer] > 255:
+    elif tape[pointer] > 255:
         tape[pointer] = 0
 
     program_pointer += 1
+
+print("\nEnd of program.")
